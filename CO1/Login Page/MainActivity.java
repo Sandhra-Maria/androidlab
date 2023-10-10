c1631761679a90731c7c539b0cc637b04946155a
Sandhra-Maria <125427765+Sandhra-Maria@users.noreply.github.com>
2023-10-10 10:53:42 +0530

    Update MainActivity.java

--- CO1/Login Page/MainActivity.java
+++ CO1/Login Page/MainActivity.java
@@ -1,28 +1,37 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
     private EditText unameEditText;
-    private EditText passEditText;
+    private  EditText passEditText;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         unameEditText = findViewById(R.id.uname1);
         passEditText = findViewById(R.id.pass1);
     }
 
-    public void ggg(View view) {
+    public void Login(View view) {
         String username = unameEditText.getText().toString();
         String password = passEditText.getText().toString();
-        if (isValidCredentials(username,password))
-        {
-            Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show();
+        if (isValidCredentials(username,password)){
+            Toast.makeText(this,"login Successful",Toast.LENGTH_SHORT).show();
+
         }
-        else
-        {
-            Toast.makeText(this, "Invalid login", Toast.LENGTH_SHORT).show();
+        else{
+            Toast.makeText(this,"inalid credentials",Toast.LENGTH_SHORT).show();
         }
     }
-
-    private boolean isValidCredentials(String username, String password) {
-        return username.equals("Sandhra") && password.equals("12345");
+    private boolean isValidCredentials(String username,String password){
+        return username.equals("sandhra") && password.equals("1234");
     }
 }
